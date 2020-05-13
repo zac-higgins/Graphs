68d0345b84edc0a67cec18e3eacbad281c595328
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-05-13 09:51:34 -0600

    copied util file into ancestor directory to resolve import issue. Completed earliest_ancestor function and added comments

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,8 +1,5 @@
-import sys
-sys.path.insert(0, '../graph')
-from util import Queue
+from util import Queue, Stack
 class Graph:
-
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
@@ -29,38 +26,58 @@ class Graph:
         return self.vertices[vertex_id]
 
 def earliest_ancestor(ancestors, starting_node):
-    # Iterate through ancestors list
-        #for each tuple:
-        #   if tuple[0] not in self.vertices, add tuple[0]
-        #   if tuple[1] not in self.vertices, add tuple[1]
-        #   self.add_edge(tuple[1], tuple[0])
-    g = Graph()
+    g = Graph() # instance of Graph
+    # Iterate through ancestors list and add all the given IDs as vertices
     for pair in ancestors:
         if pair[0] not in g.vertices:
             g.add_vertex(pair[0])
         if pair[1] not in g.vertices:
             g.add_vertex(pair[1])
-        g.add_edge(pair[1], pair[0])
+        g.add_edge(pair[1], pair[0]) # connects the vertices as indicated by given tuples
 
-    # once the graph is built, bft to explore the graph and see what we get...
-    q = Queue()
-    q.enqueue(starting_node)
-    # keep track of visited nodes
+    #------- DFT Setup -------#
+    stack = Stack()
+    stack.push(starting_node)
     visited = set()
-    # repeat until queue is empty
-    while q.size() > 0:
+    potential_paths = []
 
-        # dequeue first vert
-        current_vertex = q.dequeue()
+    def dft(starting_vertex, visited, path):
+        if starting_vertex not in visited:
+            visited.add(starting_vertex) #marks vertex as visited
+            path = path + [starting_vertex] #creates a copy of path
+            for next_vertex in g.get_neighbors(starting_vertex):
+                dft(next_vertex, visited, path) #recursive call of dft
+        # the first path found will be the longest/deepest
+        if len(potential_paths) == 0:
+            potential_paths.append(path) #add the longest path to list of potential paths
+        elif len(potential_paths) > 0:
+            # there might be a second path of equal length,
+            # we'll need to save it too in case it has a lower index
+            if len(path) == len(potential_paths[0]):
+                potential_paths.append(path)
+        # we can ignore the rest of the results of the DFT
+        # since they'll be partial or shorter paths
 
-        # if it's not visited:
-        if current_vertex not in visited:
-            visited.add(current_vertex)
-        # add neighbors to queue
-            for next_vertex in g.get_neighbors(current_vertex):
-                q.enqueue(next_vertex)
-    for vertex in visited:
-        print(vertex)
+    #------- Call DFT -------#
+    dft(starting_node, visited, potential_paths)
+
+    #------- Checks to return the correct last-node -------#
+    # potential_paths now contains 1 or 2 lists which are the longest possible paths
+    # if only one path, return the last node
+    if len(potential_paths) == 1:
+        output = potential_paths[0][-1]
+    # if two possible paths,
+    # return the last-node with the lower index value
+    else:
+        if potential_paths[0][-1] < potential_paths[1][-1]:
+            output = potential_paths[0][-1]
+        else:
+            output = potential_paths[1][-1]
+    # if last-node is same as starting-node, no ancestors
+    # return -1 as per spec
+    if output == starting_node:
+        output = -1
+    return output
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-earliest_ancestor(test_ancestors, 6)
+print(earliest_ancestor(test_ancestors, 2))
